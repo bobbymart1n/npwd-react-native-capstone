b4826e02b9d1523026644c9c6ff87b5d3200b843
Bobby Martin <robert.martin.619@gmail.com>
2018-07-13 16:44:57 -0700

    Add logo to app

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { NativeRouter, Route, Link } from 'react-router-native'
 
 
@@ -7,6 +7,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Image source={require('./assets/logo.png')}/>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
@@ -18,8 +19,11 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#C8D5B9',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: '80%'
+  }
 });
