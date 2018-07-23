28cf2ac3dd16be018c54fabcecc0ddcab10982a2
Bobby Martin <robert.martin.619@gmail.com>
2018-07-23 11:50:33 -0700

    Add static info to phone details component

--- src/components/PhoneNumberDetails.js
+++ src/components/PhoneNumberDetails.js
@@ -3,7 +3,6 @@ import { StyleSheet, View, Text } from 'react-native';
 import { connect } from 'react-redux';
 
 const PhoneNumberDetails = (props) => {
-  console.log(props.testNumber['5558675309']);
   const updatedPhoneNumber = () => {
     let areaCode = ['('];
     let firstThree = [];
@@ -14,19 +13,64 @@ const PhoneNumberDetails = (props) => {
     areaCode.push(')');
     firstThree.push('-');
     let newPhoneNumber = areaCode.concat(firstThree).concat(lastFour).join('');
-    return console.log(newPhoneNumber);
+    return newPhoneNumber;
   }
   return (
     <View>
-      <Text>
+      <Text style={styles.phoneNumber}>
         {updatedPhoneNumber()}
       </Text>
+      <View style={styles.detailsContainer}>
+        <View style={styles.phoneRep}>
+          <View style={styles.detailInfo}>
+            <Text>Reputation</Text>
+            <Text>3</Text>
+          </View>
+          <View style={styles.detailInfo}>
+            <Text>Score</Text>
+            <Text>75</Text>
+          </View>
+          <View style={styles.detailInfo}>
+            <Text>Report Count</Text>
+            <Text>8</Text>
+          </View>
+        </View>
+        <Text style={styles.scamType}>IRS Scam</Text>
+        <View style={styles.detailInfo}>
+          <Text>This number is a</Text>
+          <Text>RISK</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  phoneNumber: {
+    fontSize: 50,
+    marginBottom: 30
+  },
+  detailInfo: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  phoneRep: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  detailsContainer: {
+    display: 'flex',
+    padding: 20,
+    borderRadius: 8,
+    backgroundColor: '#FAF3DD'
+  },
+  scamType: {
+    width: '100%',
+    textAlign: 'center',
+    marginTop: 50,
+    marginBottom: 50
+  }
 });
 
 const mapStateToProps = state => {
