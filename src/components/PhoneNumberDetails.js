1a029f47dc034548d9a405641c2be0fdb10bdb9a
Bobby Martin <robert.martin.619@gmail.com>
2018-07-23 10:23:24 -0700

    Add edited phone number logic

--- src/components/PhoneNumberDetails.js
+++ src/components/PhoneNumberDetails.js
@@ -4,12 +4,22 @@ import { connect } from 'react-redux';
 
 const PhoneNumberDetails = (props) => {
   console.log(props.testNumber['5558675309']);
+  const updatedPhoneNumber = () => {
+    let areaCode = ['('];
+    let firstThree = [];
+    let lastFour = [];
+    props.testNumber['5558675309'].phoneNumber.toString().split('').map((number, index) => {
+      index < 3 ? areaCode.push(number) : index > 2 && index < 6 ? firstThree.push(number) : lastFour.push(number);
+    });
+    areaCode.push(')');
+    firstThree.push('-');
+    let newPhoneNumber = areaCode.concat(firstThree).concat(lastFour).join('');
+    return console.log(newPhoneNumber);
+  }
   return (
     <View>
       <Text>
-        {props.testNumber['5558675309'].phoneNumber.toString().split('').map((number, index) => {
-          
-        }).join('')}
+        {updatedPhoneNumber()}
       </Text>
     </View>
   );
