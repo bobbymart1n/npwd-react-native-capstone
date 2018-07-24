04c19daace4e122ce71fe5daf7ed2aa016dad4a1
Bobby Martin <robert.martin.619@gmail.com>
2018-07-24 15:33:54 -0700

    Remove fontLoader saga

--- src/sagas/sagas.js
+++ src/sagas/sagas.js
@@ -1,5 +1,5 @@
 import { takeEvery, call, put, fork } from "redux-saga/effects";
-import { requestNumber, requestNumberFailed, requestNumberSuccess, fontLoader } from './../actions';
+import { requestNumber, requestNumberFailed, requestNumberSuccess } from './../actions';
 import constants from './../constants';
 const { types } = constants;
 import { fakeData } from './../constants/dummyTest';
@@ -20,7 +20,8 @@ function* watcherTestSaga() {
 function* fetchPhoneNumber() {
   try {
     yield put(requestNumber());
-    const data = yield call(getFakeData)
+    const data = yield call(getFakeData);
+    console.log(data);
     yield put(requestNumberSuccess(data));
   } catch (error) {
     yield put(requestNumberFailed(error))
