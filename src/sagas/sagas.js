614814ce3e89611f1c3dc696fd5c4b013d8be6d2
Bobby Martin <robert.martin.619@gmail.com>
2018-07-25 15:48:14 -0700

    Update saga to fetch api

--- src/sagas/sagas.js
+++ src/sagas/sagas.js
@@ -1,10 +1,10 @@
 import { takeEvery, call, put, fork } from "redux-saga/effects";
 import { requestNumber, requestNumberFailed, requestNumberSuccess } from './../actions';
+import { apiKey } from './../constants/apiKey';
 import constants from './../constants';
 const { types } = constants;
-import { fakeData } from './../constants/dummyTest';
 
-const getFakeData = () => Promise.resolve(fakeData)
+
 
 export default function* root() {
   yield fork(watcherTestSaga)
@@ -14,10 +14,13 @@ function* watcherTestSaga() {
   yield takeEvery(types.FETCHED_NUMBER, fetchPhoneNumber);
 }
 
-function* fetchPhoneNumber() {
+function* fetchPhoneNumber(number) {
   try {
     yield put(requestNumber());
-    const data = yield call(getFakeData);
+    const data = yield call(() => {
+      return fetch('https://proapi.whitepages.com/3.0/phone_reputation?api_key=' + apiKey + '&phone.country_hint=US&phone=' + number.number).then(res => res.json());
+    });
+    console.log(data);
     yield put(requestNumberSuccess(data));
   } catch (error) {
     yield put(requestNumberFailed(error))
