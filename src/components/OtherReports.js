e03f62f6bcc29a7e9d7d88710d05bb1919c270c3
Bobby Martin <robert.martin.619@gmail.com>
2018-09-19 12:30:26 -0700

    Add reports component

---
+++ src/components/OtherReports.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const OtherReports = () => (
+  <View className={styles.reports}>Test</View>
+);
+
+const styles = StyleSheet.create({
+  reports: {
+    flex: 1
+  }
+})
+
+export default OtherReports;
