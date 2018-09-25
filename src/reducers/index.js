c4b50eefabbbbb8e16d8589f8f55aaf93d20789b
Bobby Martin <robert.martin.619@gmail.com>
2018-09-25 11:40:43 -0700

    Add new reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import reducer from './reducer';
 import navReducer from './navReducer';
 import phoneNumber from './phoneNumber';
+import results from './results';
 import { combineReducers } from 'redux';
 const rootReducer = combineReducers({
   nav: navReducer,
   phoneNumberDetails: reducer,
-  phoneNumber: phoneNumber
+  phoneNumber: phoneNumber,
+  phoneNumberResults: results
 });
 
 export default rootReducer;
