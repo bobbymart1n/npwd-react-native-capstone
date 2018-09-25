bdcecc3020c096c6b016f4f026e17d3f82984f43
Bobby Martin <robert.martin.619@gmail.com>
2018-09-25 11:40:15 -0700

    Add comments for action creators to be more readble

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,24 @@
 import constants from './../constants';
 const { types } = constants;
 
+// Starts watcherTestSaga
+export const fetchNumber = (number) => {
+  return { type: types.FETCHED_NUMBER, number: number }
+};
+// Starts initial GET Request
 export const requestNumber = () => {
   return { type: types.REQUEST_CHECK }
 };
-
+// Ends promise with requested data
 export const requestNumberSuccess = (resolve) => {
   return { type: types.CHECK_NUMBER_SUCCEEDED, data: resolve }
 };
-
+// Ends promise with error from request
 export const requestNumberFailed = () => {
   return { type: types.CHECK_NUMBER_FAILED }
 };
 
+// Stores user entered phone number
 export const phoneNumberEntry = (number) => {
   return { type: types.PHONE_NUMBER_ENTRY, number: number }
-};
-
-export const fetchNumber = (number) => {
-  return { type: types.FETCHED_NUMBER, number: number }
 };
