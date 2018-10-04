03566e289961cdff49f0d46563be2fcd408ab657
Bobby Martin <robert.martin.619@gmail.com>
2018-10-03 21:36:50 -0700

    Add state to props and render report item components

--- src/components/OtherReports.js
+++ src/components/OtherReports.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { StyleSheet, View, Text } from 'react-native';
+import ReportItem from './ReportItem';
 
 class OtherReports extends Component {
   render() {
     return (
       <View className={styles.reports}>
-        <Text>Test</Text>
+        {this.props.reports !== '' ?
+          Object.keys(this.props.reports.numbers).map(report => {
+            const reportItem = this.props.reports.numbers[report];
+            return <ReportItem
+              riskType={reportItem.riskType}
+              reportCount={reportItem.reportCount}
+              phoneNumber={reportItem.phoneNumber}
+              category={reportItem.category} />
+        }) : "Loading..."}
       </View>
     )
   }
@@ -18,4 +28,10 @@ const styles = StyleSheet.create({
   }
 })
 
-export default OtherReports;
+const mapStateToProps = state => {
+  return {
+    reports: state.phoneNumberResults.data
+  }
+}
+
+export default connect(mapStateToProps)(OtherReports);
