80935c4701c64b30477f320376e599ccdd3eeae9
Bobby Martin <robert.martin.619@gmail.com>
2018-10-03 21:38:03 -0700

    Add prop validation to other reports

--- src/components/OtherReports.js
+++ src/components/OtherReports.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { StyleSheet, View, Text } from 'react-native';
 import ReportItem from './ReportItem';
@@ -20,6 +21,10 @@ class OtherReports extends Component {
     )
   }
 };
+
+OtherReports.propTypes = {
+  reports: PropTypes.shape({})
+}
 
 const styles = StyleSheet.create({
   reports: {
