eb6f090e70ddff1ef212707706c7c406e52ee1e3
Bobby Martin <robert.martin.619@gmail.com>
2018-10-03 20:38:34 -0700

    Add reducer for results

--- src/reducers/results.js
+++ src/reducers/results.js
@@ -13,6 +13,16 @@ const results = (state = {}, action) => {
         ...state,
         data: action.data
       }
+    case types.POST_NUMBER_RESULTS:
+      return {
+        ...state,
+        data: ''
+      }
+    case types.POST_NUMBER_RESULTS_SUCCESS:
+      return {
+        ...state,
+        data: action.data
+      }
     default:
       return state;
   }
