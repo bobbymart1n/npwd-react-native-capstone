78d76b0684a5cbca57db9ee4deb1affb320bd81f
Bobby Martin <robert.martin.619@gmail.com>
2018-10-03 22:57:56 -0700

    Add styling to other reports component along with title

--- src/components/OtherReports.js
+++ src/components/OtherReports.js
@@ -1,35 +1,42 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, ScrollView, View, Text } from 'react-native';
 import ReportItem from './ReportItem';
 
 class OtherReports extends Component {
   render() {
     return (
-      <View className={styles.reports}>
+      <ScrollView
+        contentContainerStyle={styles.reports}>
+        <Text style={styles.pageTitle}>Other Reports</Text>
         {this.props.reports !== '' ?
           Object.keys(this.props.reports.numbers).map(report => {
             const reportItem = this.props.reports.numbers[report];
             return <ReportItem
+              key={report}
               riskType={reportItem.riskType}
               reportCount={reportItem.reportCount}
               phoneNumber={reportItem.phoneNumber}
               category={reportItem.category} />
         }) : "Loading..."}
-      </View>
+      </ScrollView>
     )
   }
 };
 
-OtherReports.propTypes = {
-  reports: PropTypes.string.isRequired
-}
-
 const styles = StyleSheet.create({
   reports: {
-    flex: 1,
-    marginTop: 30
+    width: '100%',
+    backgroundColor: '#F9F9F9',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexGrow: 1,
+    padding: 30,
+  },
+  pageTitle: {
+    fontSize: 36,
+    fontFamily: 'raleway-semi-bold',
+    textDecorationLine: 'underline'
   }
 })
 
